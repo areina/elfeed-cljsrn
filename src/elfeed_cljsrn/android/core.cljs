12093401c31b9f51a47589ddcd04a88de3570560
Toni Reina <areina0@gmail.com>
2016-09-16 02:09:24 -0500

    split code in scenes: settings

--- src/elfeed_cljsrn/android/core.cljs
+++ src/elfeed_cljsrn/android/core.cljs
@@ -3,8 +3,9 @@
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
             [elfeed-cljsrn.rn :as rn]
             [elfeed-cljsrn.navigation :refer [navigate-back navigate-to]]
-            [elfeed-cljsrn.ui :as ui :refer [colors palette]]
+            [elfeed-cljsrn.ui :as ui :refer [colors palette button]]
             [elfeed-cljsrn.android.scenes.configure-server :refer [configure-server-scene]]
+            [elfeed-cljsrn.android.scenes.settings :refer [settings-scene]]
             [elfeed-cljsrn.events]
             [elfeed-cljsrn.subs])
   (:import [goog.i18n DateTimeFormat]))
@@ -37,32 +38,6 @@
   [rn/view {:style {:padding 10
                     :background-color "#fff9c4"}}
    [rn/text "Network error. Check your wifi or your elfeed server."]])
-
-(defn settings-scene []
-  (let [server (subscribe [:server])
-        server-url (r/atom (:url @server))
-        styles {:wrapper {:flex 1
-                          :padding-top 10
-                          :padding-horizontal 16}
-                :button {:wrapper {:background-color (:white colors)
-                                   :margin-top 0
-                                   :padding 6
-                                   :width 106}
-                         :text {:font-weight "bold"
-                                :font-size 12
-                                :color (:dark-primary palette)}}}]
-    (fn []
-      [rn/view {:style (:wrapper styles)}
-       [rn/text "Elfeed web url: "]
-       [rn/text-input {:style {}
-                       :keyboard-type "url"
-                       :underline-color-android (when (false? (:valid? @server)) (:error palette))
-                       :on-change-text (fn [text]
-                                         (reset! server-url text)
-                                         (r/flush))
-                       :value @server-url}]
-       [button {:on-press #(dispatch [:update-server @server-url])
-                :style (:button styles)} "UPDATE SERVER"]])))
 
 (defn entry-quick-actions [entry]
   (let [styles {:wrapper {:flex 1
