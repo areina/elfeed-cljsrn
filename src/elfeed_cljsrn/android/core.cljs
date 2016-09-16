9ef2bc81439aa34d84999303e5db22072d31c34d
Toni Reina <areina0@gmail.com>
2016-09-16 02:04:55 -0500

    Create new ui ns
    Move colors, palette and button component to this new namespace.

--- src/elfeed_cljsrn/android/core.cljs
+++ src/elfeed_cljsrn/android/core.cljs
@@ -3,31 +3,10 @@
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
             [elfeed-cljsrn.rn :as rn]
             [elfeed-cljsrn.navigation :refer [navigate-back navigate-to]]
+            [elfeed-cljsrn.ui :as ui :refer [colors palette]]
             [elfeed-cljsrn.events]
             [elfeed-cljsrn.subs])
   (:import [goog.i18n DateTimeFormat]))
-
-(def colors {:white "#FFFFFF"
-             :grey50 "#FAFAFA"
-             :grey100 "#F5F5F5"
-             :grey200 "#EEEEEE"
-             :grey300 "#E0E0E0"
-             :grey400 "#BDBDBD"
-             :grey600 "#757575"
-             :grey900 "#212121"
-             :red500 "#F44336"
-             :teal100 "#B2DFDB"
-             :teal500 "#009688"
-             :teal700 "#00796B"})
-(def palette {:dark-primary (:teal700 colors)
-              :primary (:teal500 colors)
-              :light-primary (:teal100 colors)
-              :accent "#CDDC39"
-              :text (:white colors)
-              :primary-text (:grey900 colors)
-              :secondary-text (:grey600 colors)
-              :divider (:grey400 colors)
-              :error (:red500 colors)})
 
 (def MaterialIcons (js/require "react-native-vector-icons/MaterialIcons"))
 (def icon (r/adapt-react-class MaterialIcons))
@@ -52,16 +31,6 @@
 (defn format-update-time [time]
   (let [js-date (js/Date. (* time 1000))]
     (.format (goog.i18n.DateTimeFormat. "dd/MM/yyyy hh:mm") js-date)))
-
-(defn button [props label]
-  (let [styles (merge-with merge {:wrapper {:margin-top 8
-                                            :border-radius 2
-                                            :background-color (:secondary-text palette)
-                                            :padding 10}
-                                  :text {:color (:text palette)}} (:style props))]
-    [rn/touchable {:on-press (:on-press props) :disabled (:disabled? props)}
-     [rn/view {:style (:wrapper styles)}
-      [rn/text {:style (:text styles)} label]]]))
 
 (defn remote-error-message []
   [rn/view {:style {:padding 10
---
+++ src/elfeed_cljsrn/ui.cljs
@@ -0,0 +1,37 @@
+(ns elfeed-cljsrn.ui
+  (:require [reagent.core :as r :refer [atom]]
+            [elfeed-cljsrn.rn :as rn]))
+
+(def icon (r/adapt-react-class (js/require "react-native-vector-icons/MaterialIcons")))
+
+(def colors {:white "#FFFFFF"
+             :grey50 "#FAFAFA"
+             :grey100 "#F5F5F5"
+             :grey200 "#EEEEEE"
+             :grey300 "#E0E0E0"
+             :grey400 "#BDBDBD"
+             :grey600 "#757575"
+             :grey900 "#212121"
+             :red500 "#F44336"
+             :teal100 "#B2DFDB"
+             :teal500 "#009688"
+             :teal700 "#00796B"})
+(def palette {:dark-primary (:teal700 colors)
+              :primary (:teal500 colors)
+              :light-primary (:teal100 colors)
+              :accent "#CDDC39"
+              :text (:white colors)
+              :primary-text (:grey900 colors)
+              :secondary-text (:grey600 colors)
+              :divider (:grey400 colors)
+              :error (:red500 colors)})
+
+(defn button [props label]
+  (let [styles (merge-with merge {:wrapper {:margin-top 8
+                                            :border-radius 2
+                                            :background-color (:secondary-text palette)
+                                            :padding 10}
+                                  :text {:color (:text palette)}} (:style props))]
+    [rn/touchable {:on-press (:on-press props) :disabled (:disabled? props)}
+     [rn/view {:style (:wrapper styles)}
+      [rn/text {:style (:text styles)} label]]]))
