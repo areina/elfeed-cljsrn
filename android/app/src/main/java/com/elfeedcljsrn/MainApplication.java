743b6bda98c9ca1f74d7660d62fe781cdcf5b2bb
Toni Reina <areina0@gmail.com>
2016-12-27 02:41:56 +0100

    Upgrade RN to version 0.39.2

--- android/app/src/main/java/com/elfeedcljsrn/MainApplication.java
+++ android/app/src/main/java/com/elfeedcljsrn/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -33,5 +34,11 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public ReactNativeHost getReactNativeHost() {
       return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
