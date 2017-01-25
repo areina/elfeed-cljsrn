47bf8fd7bf40ef243969394c76ca6785ad2310ad
Toni Reina <areina0@gmail.com>
2017-01-26 00:01:30 +0100

    Remove empty `core_test`

--- test/elfeed_cljsrn/core_test.cljs
+++
@@ -1,6 +0,0 @@
-(ns elfeed-cljsrn.core-test
-  (:require [cljs.test :refer [deftest is testing]]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- test/runners/tests.cljs
+++ test/runners/tests.cljs
@@ -1,3 +1,2 @@
 (ns runners.tests
-  (:require [elfeed-cljsrn.core-test]
-            [elfeed-cljsrn.events-test]))
+  (:require [elfeed-cljsrn.events-test]))
