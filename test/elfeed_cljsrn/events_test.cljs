643fa612f9863db75f9359a620de08a173f064f1
Toni Reina <areina0@gmail.com>
2017-02-07 20:32:33 +0100

    Add tests for `fetch-entries` event

--- test/elfeed_cljsrn/events_test.cljs
+++ test/elfeed_cljsrn/events_test.cljs
@@ -27,3 +27,25 @@
       (is (= (events/search-execute-handler {:db db} [:search/execute {:term term}])
              {:dispatch [:fetch-entries expected-search-params]
               :db expected-db})))))
+
+(deftest fetch-entries-handler-test
+  (testing
+    "when term in search-params is empty"
+    (let [term ""
+          expected-default-term "@15-days-old +unread"
+          db {}
+          search-params {:term term :default-term expected-default-term :feed-title nil}
+          subject (events/fetch-entries {:db db} [:fetch/entries search-params])]
+      (is (= (:params (:http-xhrio subject))
+             {:q (js/encodeURIComponent expected-default-term)}))
+      (is (= (:on-success (:http-xhrio subject)) [:success-fetch-entries search-params]))
+      (is (= (:db subject) {:fetching-feeds? true :fetching-entries? true}))))
+  (testing
+    "when there is feed-title in search-params"
+    (let [search-params {:term "@15-days-old +unread" :feed-title "Foo"}
+          term-without-feed-title (:term search-params)
+          subject (events/fetch-entries {:db db} [:fetch/entries search-params])]
+      (is (= (:params (:http-xhrio subject))
+             {:q (js/encodeURIComponent term-without-feed-title)}))
+      (is (= (:on-success (:http-xhrio subject)) [:success-fetch-entries search-params]))
+      (is (= (:db subject) {:fetching-feeds? true :fetching-entries? true})))))
