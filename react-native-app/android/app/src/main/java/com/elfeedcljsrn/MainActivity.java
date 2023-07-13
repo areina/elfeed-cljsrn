c037fdb63bdd0a4dce3c883d887d47fc8b517489
Toni Reina <areina0@gmail.com>
2023-07-13 13:36:34 +0200

    Update android main activity
    There is an error in the play console I can't reproduce locally.
    However, following that they explain in the readme of this repo
    https://github.com/software-mansion/react-native-screens/#android (it's
    a dependency of this app), it's recommended to apply this change.

--- react-native-app/android/app/src/main/java/com/elfeedcljsrn/MainActivity.java
+++ react-native-app/android/app/src/main/java/com/elfeedcljsrn/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     SplashScreen.show(this);
-    super.onCreate(savedInstanceState);
+    super.onCreate(null);
   }
 
   /**
